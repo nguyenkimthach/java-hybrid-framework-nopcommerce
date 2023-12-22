d846083f1d19bb5fa5b949a9e09bd0b64f3f1709
Nguyen Kim Thach <nguyenkimthach.a@gmail.com>
2023-12-22 16:38:12 +0700

    AddNew class UserNoteBookPageObject, UserNoteBookPageUI

---
+++ interfaces/pageUIs/nopCommerce/user/UserNoteBookPageUI.java
@@ -0,0 +1,11 @@
+package pageUIs.nopCommerce.user;
+
+public class UserNoteBookPageUI {
+	public static final String SORTBY_DROPDOWN = "css=select#products-orderby";
+	public static final String ALL_PRODUCT_NAME = "css=h2.product-title>a";
+	public static final String ALL_PRODUCT_Price = "css=div.prices>span.actual-price";
+	public static final String DISPLAY_DROPDOWN = "css=select#products-pagesize";
+	public static final String SELECT_PAGE_BY_TEXT_NUMBER = "xpath=//div[@class='pager']//a[@data-page='%s'][1]";
+	public static final String ICON_NEXT_PAGE = "css=li.next-page>a";
+	public static final String ICON_PREVIOUS_PAGE = "css=li.previous-page>a";
+}
