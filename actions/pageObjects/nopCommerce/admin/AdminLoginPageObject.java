8efc2c85e5112ebffe93e257d6417c8bff82b382
Nguyen Kim Thach <nguyenkimthach.a@gmail.com>
2023-12-27 21:01:10 +0700

    Add new class AdminLoginPageObject AdminLoginPageUI

---
+++ actions/pageObjects/nopCommerce/admin/AdminLoginPageObject.java
@@ -0,0 +1,37 @@
+package pageObjects.nopCommerce.admin;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import commons.PageGeneratorManage;
+import pageUIs.nopCommerce.admin.AdminLoginPageUI;
+
+public class AdminLoginPageObject extends BasePage {
+	private WebDriver driver;
+
+	public AdminLoginPageObject(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+	}
+
+	public void inPutToEmailTextbox(String emailAddress) {
+		waitForElementVisible(AdminLoginPageUI.EMAIL_TEXTBOX);
+		senkeyToElement(AdminLoginPageUI.EMAIL_TEXTBOX, emailAddress);
+	}
+
+	public void inPutToPasswordTextbox(String password) {
+		waitForElementVisible(AdminLoginPageUI.PASSWORD_TEXTBOX);
+		senkeyToElement(AdminLoginPageUI.PASSWORD_TEXTBOX, password);
+	}
+
+	public void selectRememberCheckbox() {
+		waitForElementClickable(AdminLoginPageUI.REMEMBER_ME_CHECKBOX);
+		checkToDefaultCheckboxOrRadio(AdminLoginPageUI.REMEMBER_ME_CHECKBOX);
+	}
+
+	public AdminDashboardPageObject clickToRegisterButton() {
+		waitForElementClickable(AdminLoginPageUI.LOGIN_BUTTON);
+		clickToElement(AdminLoginPageUI.LOGIN_BUTTON);
+		return PageGeneratorManage.getAdminDashboardPage(driver);
+	}
+}
---
+++ interfaces/pageUIs/nopCommerce/admin/AdminLoginPageUI.java
@@ -0,0 +1,8 @@
+package pageUIs.nopCommerce.admin;
+
+public class AdminLoginPageUI {
+	public static final String EMAIL_TEXTBOX = "css=input#Email";
+	public static final String PASSWORD_TEXTBOX = "css=input#Password";
+	public static final String REMEMBER_ME_CHECKBOX = "css=input#RememberMe";
+	public static final String LOGIN_BUTTON = "css=button.login-button";
+}
